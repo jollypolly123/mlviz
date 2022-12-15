4b38a36f8ab9183f6e6ecb836ed80a30e7236da0
Rachelle Hu <rachelle.hu123@gmail.com>
2022-12-14 23:05:47 -0500

    initialize sveltekit
    begin mdp page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About this app</h1>
+	
+	<p>
+		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
+		using it with JavaScript disabled!
+	</p>
+
+	<p>
+		The <a href="/markov-decision-processes">Markov Decision Processes</a> page is an interactive visualization tool to practice your understanding of MDPs.
+	</p>
+</div>
---
+++ src/routes/markov-decision-processes/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Markov Decision Processes</title>
+	<meta name="description" content="A visualization tool for Markov Decision Processes (MDPs)" />
+</svelte:head>
+
+<div>
+    <h1>Markov Decision Process Visualization</h1>
+
+    <p>
+        This tool allows you to visualize Markov Decision Processes (MDPs) and their value functions.
+    </p>
+
+    <form>
+        <h2>MDP Definition</h2>
+        <input type="text" name="states" placeholder="States" />
+        <input type="text" name="transitions" placeholder="Transitions" />
+        <input type="text" name="actions" placeholder="Actions" />
+        <input type="text" name="rewards" placeholder="Rewards" />
+        
+        <h2>Training Hyperparameters</h2>
+        <input type="text" name="discount" placeholder="Discount value (gamma)" />
+        <input type="text" name="learning" placeholder="Learning rate" />
+        <input type="submit" value="Submit" />
+    </form>
+
+    <h3>Generated graph</h3>
+    
+    <h3>Calculate values</h3>
+    <form>
+        <input type="text" name="horizon" placeholder="Horizon" />
+
+        <input type="text" name="iterations" placeholder="Number of iterations" />
+        <input type="submit" value="Submit" />
+    </form>
+</div>
